6971359a0d924e4c3040dfd1e8e5c471e8499565
davRi67 <dav.rinie@gmail.com>
2026-06-30 15:44:21 +0200

    test: make test_health assertions executable

--- home-server/tests/test_main.py
+++ home-server/tests/test_main.py
@@ -8,3 +8,5 @@ def test_health():
     response = client.get("/health")
     # assertion 1 : le status_code doit valoir 200
     # assertion 2 : le json() doit valoir {"status": "healthy"}
+    assert response.status_code == 200
+    assert response.json() == {"status": "healthy"}
